128f0d3c5fb7d3ead0f499a324423c167eb96d77
Zied-bousnina <zbousnina@yahoo.com>
2023-05-06 00:03:11 +0100

    first commit

---
+++ src/components/Navbars/DemoNavbar.js
@@ -0,0 +1,281 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// JavaScript plugin that hides or shows a component based on your scroll
+import Headroom from "headroom.js";
+// reactstrap components
+import {
+  Button,
+  UncontrolledCollapse,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class DemoNavbar extends React.Component {
+  componentDidMount() {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    // initialise
+    headroom.init();
+  }
+  state = {
+    collapseClasses: "",
+    collapseOpen: false
+  };
+
+  onExiting = () => {
+    this.setState({
+      collapseClasses: "collapsing-out"
+    });
+  };
+
+  onExited = () => {
+    this.setState({
+      collapseClasses: ""
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <header className="header-global">
+          <Navbar
+            className="navbar-main navbar-transparent navbar-light headroom"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+                <img
+                  alt="..."
+                  style={{width: "200px", height: "auto"}}
+                  
+                  src={"https://xgenbox.com/wp-content/uploads/2023/03/logo-copie.png"}
+                />
+              </NavbarBrand>
+              <button className="navbar-toggler" id="navbar_global">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse
+                toggler="#navbar_global"
+                navbar
+                className={this.state.collapseClasses}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+              >
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <Link to="/">
+                        <img
+                          alt="..."
+                          src={require("assets/img/brand/argon-react.png")}
+                        />
+                      </Link>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+              
+
+
+
+                {/*  */}
+                <Nav className="align-items-lg-center ml-lg-auto  " navbar>
+                  
+
+
+
+
+
+
+
+
+
+
+
+                <Nav className="navbar-nav-hover align-items-lg-center  " navbar>
+                  {/* <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-ui-04 d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Components</span>
+                    </DropdownToggle>
+                    <DropdownMenu className="dropdown-menu-xl">
+                      <div className="dropdown-menu-inner">
+                        <Media
+                          className="d-flex align-items-center"
+                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/overview?ref=adsr-navbar"
+                          target="_blank"
+                        >
+                          <div className="icon icon-shape bg-gradient-primary rounded-circle text-white">
+                            <i className="ni ni-spaceship" />
+                          </div>
+                          <Media body className="ml-3">
+                            <h6 className="heading text-primary mb-md-1">
+                              Getting started
+                            </h6>
+                            <p className="description d-none d-md-inline-block mb-0">
+                              Learn how to use Argon compiling Scss, change
+                              brand colors and more.
+                            </p>
+                          </Media>
+                        </Media>
+                        <Media
+                          className="d-flex align-items-center"
+                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/colors?ref=adsr-navbar"
+                          target="_blank"
+                        >
+                          <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
+                            <i className="ni ni-palette" />
+                          </div>
+                          <Media body className="ml-3">
+                            <h6 className="heading text-primary mb-md-1">
+                              Foundation
+                            </h6>
+                            <p className="description d-none d-md-inline-block mb-0">
+                              Learn more about colors, typography, icons and the
+                              grid system we used for Argon.
+                            </p>
+                          </Media>
+                        </Media>
+                        <Media
+                          className="d-flex align-items-center"
+                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alert?ref=adsr-navbar"
+                          target="_blank"
+                        >
+                          <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                            <i className="ni ni-ui-04" />
+                          </div>
+                          <Media body className="ml-3">
+                            <h5 className="heading text-warning mb-md-1">
+                              Components
+                            </h5>
+                            <p className="description d-none d-md-inline-block mb-0">
+                              Browse our 50 beautiful handcrafted components
+                              offered in the Free version.
+                            </p>
+                          </Media>
+                        </Media>
+                      </div>
+                    </DropdownMenu>
+                  </UncontrolledDropdown> */}
+
+                  {/* Products */}
+                  <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Products</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="/landing-page" tag={Link}>
+                        Smart box
+                      </DropdownItem>
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Fleet Management platform
+                      </DropdownItem>
+                      
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+
+                 {/* Markets */}
+                  <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Markets</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="/landing-page" tag={Link}>
+                        Smart cities
+                      </DropdownItem>
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Commercial establishments
+                      </DropdownItem>
+                      <DropdownItem to="/login-page" tag={Link}>
+                        private organizations
+                      </DropdownItem>
+                      <DropdownItem to="/register-page" tag={Link}>
+                        Waste collectors
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                 
+                 {/* Company */}
+                 <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Company</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="/landing-page" tag={Link}>
+                        About Company
+                      </DropdownItem>
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Partnership opportunities
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+
+                  {/* Contact */}
+                  <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">Contact</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="/landing-page" tag={Link}>
+                        Contact details
+                      </DropdownItem>
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Quote request
+                      </DropdownItem>
+                      <DropdownItem to="/login-page" tag={Link}>
+                        Technical assistance
+                      </DropdownItem>
+                      
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+
+                </Nav>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </header>
+      </>
+    );
+  }
+}
+
+export default DemoNavbar;
